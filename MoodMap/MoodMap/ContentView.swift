b6d891ea18b98e7a8b7fce56a2a6a5d03155017f
Breno Marques <brenomarques516@gmail.com>
2025-10-13 14:24:14 -0300

    :tada: init: first commit

---
+++ MoodMap/MoodMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodMap
+//
+//  Created by Breno Marques on 13/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodMap/MoodMap/MoodMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapApp.swift
+//  MoodMap
+//
+//  Created by Breno Marques on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
