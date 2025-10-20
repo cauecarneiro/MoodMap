44d4358b2b4a1160643a97b82024a990e7778856
Breno Marques <brenomarques516@gmail.com>
2025-10-20 16:55:20 -0300

    :sparkles: feat(Integration): Notification integrated to screen

--- MoodMap/MoodMap/Presentation/Views/LocationMapView.swift
+++ MoodMap/MoodMap/Presentation/Views/LocationMapView.swift
@@ -10,41 +10,77 @@ import SwiftUI
 import MapKit
 
 struct LocationMapView: View {
+    @StateObject private var notificationManager = NotificationManager()
+    @State private var notificationStatus: NotificationStatus = .notAllowed
+    
     let userLocationRequest = UserLocationManager()
     
     let academyPoint = MapAnnotationsModel(localName: "Apple Developer Academy UCB", localCoordinates: .academy, localSFSymbol: "apple.logo")
     let ucbPoint = MapAnnotationsModel(localName: "Universidade Católica de Brasília", localCoordinates: .ucb, localSFSymbol: "book.fill")
         
     var body: some View {
-        Map {
-            Annotation(academyPoint.localName, coordinate: academyPoint.localCoordinates) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 6)
-                        .fill(.background)
-                    Image(systemName: academyPoint.localSFSymbol)
-                        .foregroundStyle(.blue)
-                        .padding(8)
+        NavigationView {
+            Map {
+                Annotation(academyPoint.localName, coordinate: academyPoint.localCoordinates) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 6)
+                            .fill(.background)
+                        Image(systemName: academyPoint.localSFSymbol)
+                            .foregroundStyle(.blue)
+                            .padding(8)
+                    }
                 }
+                
+                Annotation(ucbPoint.localName, coordinate: ucbPoint.localCoordinates) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 6)
+                            .fill(.background)
+                        Image(systemName: ucbPoint.localSFSymbol)
+                            .foregroundStyle(.gray)
+                            .padding(8)
+                    }
+                }
+            }
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView()
+            }
+            .onAppear {
+                userLocationRequest.requestUserLocation()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        handleNotifications()
+                    } label: {
+                        Label("Notifications", systemImage: notificationStatus.image)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+extension LocationMapView {
+    func handleNotifications() {
+        Task {
+            if !notificationManager.allowedNotifications {
+                await notificationManager.requestAuthorizationToNotifications()
+                notificationStatus = .unsubscribed
             }
             
-            Annotation(ucbPoint.localName, coordinate: ucbPoint.localCoordinates) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 6)
-                        .fill(.background)
-                    Image(systemName: ucbPoint.localSFSymbol)
-                        .foregroundStyle(.gray)
-                        .padding(8)
-                }
+            if !notificationManager.subscribedToNotification {
+                await notificationManager.subscribeToNotification(
+                    recordType: GlobalValues.recordType,
+                    subscriptionID: GlobalValues.subscriptionID
+                )
+                notificationStatus = .subscribed
+            } else {
+                await notificationManager.unsubscribeToNotification(subscriptionID: GlobalValues.subscriptionID)
+                notificationStatus = .unsubscribed
             }
-        }
-//        .mapStyle(.standard (elevation: .realistic))
-        .mapControls {
-            MapUserLocationButton()
-            MapCompass()
-            MapScaleView()
-        }
-        .onAppear {
-            userLocationRequest.requestUserLocation()
         }
     }
 }
